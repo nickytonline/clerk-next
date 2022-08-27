9e194ba5b1eef5efffa241637d1018a3c8fedddd
Nick Taylor <nick@iamdeveloper.com>
2022-08-27 00:15:03 -0400

    added Clerk provider and components to the project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignIn,
+  UserButton,
+} from '@clerk/nextjs';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ClerkProvider>
+      <SignedIn>
+        <UserButton />
+        <Component {...pageProps} />
+      </SignedIn>
+      <SignedOut>
+        <SignIn />
+      </SignedOut>
+    </ClerkProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
