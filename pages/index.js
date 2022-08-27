e3ac617ee31dcc297c6497c3e491800b2a366ed3
Nick Taylor <nick@iamdeveloper.com>
2022-08-27 00:15:20 -0400

    added usage of some Clerk hooks

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { useAuth, useSession, useUser } from '@clerk/nextjs';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const auth = useAuth();
+  const { isLoaded, isSignedIn, user } = useUser();
+  const session = useSession();
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,9 +17,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>Welcome {user.firstName}</h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
@@ -65,5 +68,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
